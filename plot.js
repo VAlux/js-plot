e9149a9055f2185ed71e2d4882ed03189ba3a788
VALux <olex3000@yandex.ru>
2019-04-24 22:46:02 +0300

    some fancy periodic animation

--- plot.js
+++ plot.js
@@ -1,55 +1,60 @@
-var gridSize = 50
+var gridSize = 50;
 var tickLength = 3;
-var tickTextOffset = 3
-var tickTextMaxWidth = 15
+var tickTextOffset = 3;
+var tickTextMaxWidth = 15;
 
-var font = '9px Arial'
-var axisColor = "#000000"
-var tickColor = "#000000"
-var gridColor = "#e9e9e9"
+var font = "9px Arial";
+var axisColor = "#000000";
+var tickColor = "#000000";
+var gridColor = "#e9e9e9";
+var crosshairColor = "#e5e5e5";
 
-var canvas = document.getElementById("plot-canvas")
-var context = canvas.getContext('2d')
-var canvasWidth = canvas.width
-var canvasHeight = canvas.height
+var canvas = document.getElementById("plot-canvas");
+var context = canvas.getContext("2d");
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
 
-var xAxisGridLinesAmount = Math.floor(canvasHeight / gridSize)
-var yAxisGridLinesAmount = Math.floor(canvasWidth / gridSize)
+var xAxisGridLinesAmount = Math.floor(canvasWidth / gridSize);
+var yAxisGridLinesAmount = Math.floor(canvasHeight / gridSize);
 
-var xAxisGridOffset = yAxisGridLinesAmount / 2;
-var yAxisGridOffset = xAxisGridLinesAmount / 2;
+var xAxisGridOrigin = xAxisGridLinesAmount / 2;
+var yAxisGridOrigin = yAxisGridLinesAmount / 2;
 
-function initContext() {
-  context.lineCap = "round"
+var mousePosition = { x: 0, y: 0 };
+
+function init() {
+  context.lineCap = "round";
+  translateCanvasOrigin();
+  window.requestAnimationFrame(draw);
 }
 
 function translateCanvasOrigin() {
-  context.translate(yAxisGridOffset * gridSize, xAxisGridOffset * gridSize);
+  context.translate(xAxisGridOrigin * gridSize, yAxisGridOrigin * gridSize);
 }
 
 function obtainStrokeStyle(axisOrigin, currentIteration) {
   if (currentIteration === axisOrigin) {
-    return axisColor
+    return axisColor;
   } else {
-    return gridColor
+    return gridColor;
   }
 }
 
 function drawXAxis() {
   for (var i = -xAxisGridLinesAmount; i <= xAxisGridLinesAmount; i++) {
-    context.beginPath()
-    context.lineWidth = 1
+    context.beginPath();
+    context.lineWidth = 1;
 
-    context.strokeStyle = obtainStrokeStyle(0, i)
+    context.strokeStyle = obtainStrokeStyle(0, i);
 
     if (i === xAxisGridLinesAmount) {
-      const y = gridSize * i + 0.5
-      context.moveTo(-xAxisGridOffset * gridSize, y)
-      context.lineTo(canvasWidth, y)
+      const y = gridSize * i + 0.5;
+      context.moveTo(-xAxisGridOrigin * gridSize, y);
+      context.lineTo(canvasWidth, y);
     } else {
-      const y = gridSize * i
-      context.moveTo(-xAxisGridOffset * gridSize, y)
-      context.lineTo(canvasWidth, y)
+      const y = gridSize * i;
+      context.moveTo(-xAxisGridOrigin * gridSize, y);
+      context.lineTo(canvasWidth, y);
     }
 
     context.stroke();
@@ -58,19 +63,19 @@ function drawXAxis() {
 
 function drawYAxis() {
   for (var i = -yAxisGridLinesAmount; i <= yAxisGridLinesAmount; i++) {
-    context.beginPath()
-    context.lineWidth = 1
+    context.beginPath();
+    context.lineWidth = 1;
 
-    context.strokeStyle = obtainStrokeStyle(0, i)
+    context.strokeStyle = obtainStrokeStyle(0, i);
 
     if (i === yAxisGridLinesAmount) {
-      const x = gridSize * i - 0.5
-      context.moveTo(x, -yAxisGridOffset * gridSize)
-      context.lineTo(x, canvasHeight)
+      const x = gridSize * i - 0.5;
+      context.moveTo(x, -yAxisGridOrigin * gridSize);
+      context.lineTo(x, canvasHeight);
     } else {
-      const x = gridSize * i
-      context.moveTo(x, -yAxisGridOffset * gridSize)
-      context.lineTo(x, canvasHeight)
+      const x = gridSize * i;
+      context.moveTo(x, -yAxisGridOrigin * gridSize);
+      context.lineTo(x, canvasHeight);
     }
 
     context.stroke();
@@ -78,64 +83,99 @@ function drawYAxis() {
 }
 
 function drawXAxisTicks() {
-  for (var i = -xAxisGridOffset; i <= xAxisGridOffset; i++) {
-    context.beginPath()
-    context.lineWidth = 1
-    context.strokeStyle = tickColor
+  for (var i = -xAxisGridOrigin; i <= xAxisGridOrigin; i++) {
+    context.beginPath();
+    context.lineWidth = 1;
+    context.strokeStyle = tickColor;
 
-    var x = i * gridSize
-    context.moveTo(x, -tickLength)
-    context.lineTo(x, tickLength)
-    context.stroke()
+    var x = i * gridSize;
+    context.moveTo(x, -tickLength);
+    context.lineTo(x, tickLength);
+    context.stroke();
 
     if (i !== 0) {
-      context.font = font
-      context.textAlign = 'start'
-      context.fillText(i, x, -tickTextOffset, tickTextMaxWidth)
+      context.font = font;
+      context.textAlign = "start";
+      context.fillText(i, x, -tickTextOffset, tickTextMaxWidth);
     }
   }
 }
 
 function drawYAxisTicks() {
-  for (var i = -yAxisGridOffset; i <= yAxisGridOffset; i++) {
-    context.beginPath()
-    context.lineWidth = 1
-    context.strokeStyle = tickColor
+  for (var i = -yAxisGridOrigin; i <= yAxisGridOrigin; i++) {
+    context.beginPath();
+    context.lineWidth = 1;
+    context.strokeStyle = tickColor;
 
-    var y = i * gridSize
-    context.moveTo(-tickLength, y)
-    context.lineTo(tickLength, y)
-    context.stroke()
+    var y = i * gridSize;
+    context.moveTo(-tickLength, y);
+    context.lineTo(tickLength, y);
+    context.stroke();
 
     if (i !== 0) {
-      context.font = font
-      context.textAlign = 'end'
-      context.fillText(i, -tickTextOffset, y, tickTextMaxWidth)
+      context.font = font;
+      context.textAlign = "end";
+      context.fillText(i, -tickTextOffset, y, tickTextMaxWidth);
     }
   }
 }
 
+function drawCrosshair() {
+  context.beginPath();
+  context.lineWidth = 1;
+  context.strokeStyle = crosshairColor;
+  context.moveTo(-xAxisGridOrigin * gridSize, mousePosition.y);
+  context.lineTo(xAxisGridOrigin * gridSize, mousePosition.y);
+  context.stroke();
+  context.moveTo(mousePosition.x, -yAxisGridOrigin * gridSize);
+  context.lineTo(mousePosition.x, yAxisGridOrigin * gridSize);
+  context.stroke();
+  context.font = font;
+  context.textAlign = "end";
+  context.fillText(`x ${mousePosition.x} y ${mousePosition.y}`, mousePosition.x, mousePosition.y);
+}
+
+// ========================================================================
+// TODO: this is just for testing. should be deleted!
+var offset = 0.00005;
+var increment = offset;
+
 function drawFunctions(discretizationStep) {
-  for (var x = -xAxisGridOffset; x <= xAxisGridOffset; x += discretizationStep) {
-    functionIteration((arg) => Math.sin(arg), x, discretizationStep)
-    functionIteration((arg) => Math.cos(arg), x, discretizationStep)
-    functionIteration((arg) => Math.tan(arg), x, discretizationStep)
-    functionIteration((arg) => Math.atan(arg), x, discretizationStep)
+  for (var x = -xAxisGridOrigin;x <= xAxisGridOrigin; x += discretizationStep) {
+    functionIteration(arg => Math.sin(arg * offset), x, discretizationStep);
+    functionIteration(arg => Math.cos(arg * offset), x, discretizationStep);
+    functionIteration(arg => Math.tan(arg * offset), x, discretizationStep);
+    functionIteration(arg => Math.atan(arg * offset), x, discretizationStep);
+    offset += increment;
+  }
+
+  function functionIteration(func, x, step) {
+    context.beginPath();
+    context.lineWidth = 2;
+    context.moveTo(x * gridSize, func(x) * gridSize);
+    context.lineTo((x + step) * gridSize, func(x + step) * gridSize);
+    context.stroke();
   }
 }
+// ========================================================================
 
-function functionIteration(func, x, step) {
-  context.beginPath()
-  context.lineWidth = 2
-  context.moveTo(x * gridSize, func(x) * gridSize)
-  context.lineTo((x + step) * gridSize, func(x + step) * gridSize)
-  context.stroke()
+function calculateMouseRelativePositionInCanvas(e) {
+  mousePosition.x = e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft) - canvas.offsetLeft - xAxisGridOrigin * gridSize;
+  mousePosition.y = e.clientY + (document.documentElement.scrollTop || document.body.scrollTop) - canvas.offsetTop - yAxisGridOrigin * gridSize;
+  console.log("mouse position: " + JSON.stringify(mousePosition));
 }
 
-initContext()
-translateCanvasOrigin()
-drawXAxis()
-drawYAxis()
-drawXAxisTicks()
-drawYAxisTicks()
-drawFunctions(0.1)
+function draw() {
+  context.clearRect(-xAxisGridOrigin * gridSize, -yAxisGridOrigin * gridSize, canvasWidth, canvasHeight);
+
+  drawXAxis();
+  drawYAxis();
+  drawXAxisTicks();
+  drawYAxisTicks();
+  drawFunctions(0.1);
+  drawCrosshair();
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
